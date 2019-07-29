c0550da048a14cb6da1ad340793e61b99fd29b6a
Olivier Guilleux <bonjour@olivier-guilleux.com>
2019-07-29 11:47:32 +0200

    changed webpack config so that eslint works

--- gulp/core/config/scripts.js
+++ gulp/core/config/scripts.js
@@ -15,77 +15,83 @@ var assets = require('./common').paths.assets;
  * @type {{}}
  */
 module.exports = deepMerge({
-    paths: {
-        watch: assets.src + '/js/**/*.js',
-        src: [
-            assets.src + '/js/*.js',
-            '!' + assets.src + '/js/**/_*'
-        ],
-        dest: assets.dest + '/js',
-        clean: assets.dest + '/js/**/*.{js,map}'
-    },
+	paths: {
+		watch: assets.src + '/js/**/*.js',
+		src: [
+			assets.src + '/js/*.js',
+			'!' + assets.src + '/js/**/_*'
+		],
+		dest: assets.dest + '/js',
+		clean: assets.dest + '/js/**/*.{js,map}'
+	},
 
-    options: {
-        webpack: {
+	options: {
+		webpack: {
 
-            // merged with defaults
-            // for :watch task
-            watch: {
-            	mode: 'development',
-                cache: true,
-                watch: true,
-                devtool: 'eval'
-            },
-
-
-            // merged with defaults
-            // for :dev task
-            dev: {
-	            mode: 'development',
-	            devtool: 'eval'
-            },
+			// merged with defaults
+			// for :watch task
+			watch: {
+				mode: 'development',
+				cache: true,
+				watch: true,
+				devtool: 'eval'
+			},
 
 
-            // merged with defaults
-            // for :prod task
-            prod: {
-	            mode: 'production',
-	            plugins: [
-                    new webpack.optimize.OccurrenceOrderPlugin(true),
-                ]
-            },
+			// merged with defaults
+			// for :dev task
+			dev: {
+				mode: 'development',
+				devtool: 'eval'
+			},
 
-            defaults: {
-                resolve: {
-                    extensions: ['.js', '.jsx']
-                },
-                output: {
-                    chunkFilename: 'chunk-[name].js'
-                },
-                stats: {
-                    colors: true
-                },
-                plugins: [
-	                new webpack.LoaderOptionsPlugin({
-		                test: /\.jsx?$/,
-		                exclude: [
-			                /node_modules/,
-			                /bower_components/,
-			                /polyfills/
-		                ],
-		                loader: ['eslint', 'babel'],
-		                query: {
-			                presets: ['env'],
-			                plugins: ['transform-runtime']
-		                }
-					}),
-                    new webpack.ProvidePlugin({
-                        $: 'jquery',
-                        jQuery: 'jquery',
-                        'window.jQuery': 'jquery'
-                    })
-                ]
-            }
-        }
-    }
+
+			// merged with defaults
+			// for :prod task
+			prod: {
+				mode: 'production',
+				plugins: [
+					new webpack.optimize.OccurrenceOrderPlugin(true),
+				]
+			},
+
+			defaults: {
+				resolve: {
+					extensions: ['.js', '.jsx']
+				},
+				output: {
+					chunkFilename: 'chunk-[name].js'
+				},
+				stats: {
+					colors: true
+				},
+				module: {
+					rules: [
+						{
+							enforce: 'pre',
+							test: /\.js$/,
+							exclude: /node_modules/,
+							loader: 'eslint-loader'
+						},
+						{
+							test: /\.js$/,
+							exclude: /node_modules/,
+							loader: 'babel-loader',
+							options: {
+								presets: ['env'],
+								plugins: ['transform-runtime']
+							}
+						}
+					]
+				},
+				plugins: [
+					new webpack.ProvidePlugin({
+						$: 'jquery',
+						jQuery: 'jquery',
+						'window.jQuery': 'jquery'
+					})
+				]
+			}
+		}
+	}
 });

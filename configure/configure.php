ce4a64ae9e98e7550bf69646b9b4d834c5463ce1
Olivier Guilleux <bonjour@olivier-guilleux.com>
2021-05-19 09:23:09 +0200

    linting php files

--- configure/configure.php
+++ configure/configure.php
@@ -1,8 +1,7 @@
 <?php
 
 // MENUS
-function _custom_theme_register_menu()
-{
+function _custom_theme_register_menu() {
     register_nav_menus(
         array(
             'menu-main' => __( 'Menu principal' ),
@@ -66,7 +65,7 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 // delete wp-embed.js from footer
-function my_deregister_scripts(){
+function my_deregister_scripts() {
 	wp_deregister_script( 'wp-embed' );
 }
 add_action( 'wp_footer', 'my_deregister_scripts' );
@@ -89,7 +88,7 @@ function clean_header() {
 add_action('wp_enqueue_scripts', 'clean_header');
 
 // add SVG to allowed file uploads
-function add_file_types_to_uploads($mime_types){
+function add_file_types_to_uploads($mime_types) {
 	$mime_types['svg'] = 'image/svg+xml';
 
 	return $mime_types;
--- configure/js-css.php
+++ configure/js-css.php
@@ -1,13 +1,11 @@
 <?php
 
-function _add_javascript()
-{
-	wp_enqueue_script('theme-js', get_template_directory_uri() . '/assets/dist/js/main.js', array(), null, true );
+function _add_javascript() {
+	wp_enqueue_script('theme', get_template_directory_uri() . '/assets/dist/js/main.js', array(), null, true );
 }
 add_action('wp_enqueue_scripts', '_add_javascript', 100);
 
-function _add_stylesheets()
-{
-	wp_enqueue_style('theme-css', get_template_directory_uri() . '/assets/dist/css/main.css', array(), null, 'all' );
+function _add_stylesheets() {
+	wp_enqueue_style('theme', get_template_directory_uri() . '/assets/dist/css/main.css', array(), null, 'all' );
 }
 add_action('wp_enqueue_scripts', '_add_stylesheets');

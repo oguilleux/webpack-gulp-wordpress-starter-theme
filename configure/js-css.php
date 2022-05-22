b04163dc0698faa6930a8cf213e631915f62f2b5
Olivier Guilleux <bonjour@olivier-guilleux.com>
2022-05-22 22:50:13 +0200

    updated browser sync js version

--- configure/js-css.php
+++ configure/js-css.php
@@ -16,7 +16,7 @@ function add_browsersync_script() {
 	}
 	?>
 	<script id="__bs_script__">//<![CDATA[
-		document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.14'><\/script>".replace("HOST", location.hostname));
+		document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.7'><\/script>".replace("HOST", location.hostname));
 		//]]></script>
 	<?php
 }

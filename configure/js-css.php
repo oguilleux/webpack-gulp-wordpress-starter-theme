0c23f2094990de5581b2a4451472808125466ca4
Olivier Guilleux <bonjour@olivier-guilleux.com>
2022-12-13 10:47:07 +0100

    removing all WP css files enqueued by default

--- configure/js-css.php
+++ configure/js-css.php
@@ -1,11 +1,18 @@
 <?php
 
 function _add_javascript() {
-	wp_enqueue_script('theme', get_template_directory_uri() . '/assets/dist/js/main.js', array(), null, true );
+	wp_enqueue_script('theme', get_template_directory_uri() . '/assets/dist/js/main.js', null, null, true );
 }
 add_action('wp_enqueue_scripts', '_add_javascript', 100);
 
 function _add_stylesheets() {
-	wp_enqueue_style('theme', get_template_directory_uri() . '/assets/dist/css/main.css', array(), null, 'all' );
+    // removing all WP css files enqueued by default
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-block-style');
+    wp_dequeue_style('global-styles');
+    wp_dequeue_style('classic-theme-styles');
+
+	wp_enqueue_style('theme', get_template_directory_uri() . '/assets/dist/css/main.css', null, null, 'all' );
 }
 add_action('wp_enqueue_scripts', '_add_stylesheets');
